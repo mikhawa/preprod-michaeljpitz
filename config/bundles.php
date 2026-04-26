f0a753d3e798e95dd959b379fa33b8c10af4664c
mikhawa <michaeljpitz@gmail.com>
2026-04-26 17:18:20 +0200

    fix fixture

--- config/bundles.php
+++ config/bundles.php
@@ -18,5 +18,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
 ];
